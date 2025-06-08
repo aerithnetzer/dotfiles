1d63adcde150146883b83a0443d6a7106baa34a4
Aerith Netzer <aerithnetzer@outlook.com>
2025-06-08 15:35:28 -0500

    Update plist

--- run-papis-serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ensure the PATH includes pipx-installed binaries
-export PATH="$HOME/.local/bin:$PATH"
-
-# Start papis serve
-exec papis serve
---
+++ start_papis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export HOME=/Users/ysc4337
+export PATH="$HOME/.local/bin:$PATH"
+
+exec papis serve
